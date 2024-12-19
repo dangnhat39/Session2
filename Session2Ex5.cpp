3b22ebefaa32b5ea58cb7f7dc079476309e3088c
dangnhat39 <nhathat111@gmail.com>
2024-12-19 15:43:01 +0700

    Update Session2Ex5.cpp

--- Session2Ex5.cpp
+++ Session2Ex5.cpp
@@ -1,15 +1,15 @@
 #include <stdio.h>
 int main(){
-	//Khoi tao cac bien chieu dai va chieu rong
-	int length = 10;
-	//Chieu dai cua hinh chu nhat la 4cm
-	int width = 3;//Chieu rong cua hinh chu nhat la 3cm
 	
-	//Tinh chu vi cua hinh chu nhat
+	int length = 10;
+	
+	int width = 3;
+	
+	
 	int perimeter = 2* (length+width);
 	int area = length * width;
 	
-	//Hien thi ket qua
+	
 	printf("Chieu dai cua hinh chu nhat la: %d\n", length);
 	printf("Chieu rong cu hinh chu nhat la: %d\n", width);
 	printf("Chu vi cua hinh chu nhat la: %d\n", perimeter);
