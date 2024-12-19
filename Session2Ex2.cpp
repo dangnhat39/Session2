413703c295f5431f4dc3fcd767eab426f4037aa0
dangnhat39 <nhathat111@gmail.com>
2024-12-19 15:41:59 +0700

    Update Session2Ex2.cpp

--- Session2Ex2.cpp
+++ Session2Ex2.cpp
@@ -1,13 +1,12 @@
 #include <stdio.h>
 int main(){
-	//Khai bao va khoi tao so nguye
-	int num1 = 10;//So nguyen thu nhat
-	int num2 = 20;//So nguyen thu hai
+
+	int num1 = 10;
+	int num2 = 20;
 	
-	// Tong hai so nguyen la
+	
 	int sum = num1 + num2;
 	
-	//ket qua 2 so la 
 	printf("So nguyen thu nhat la: %d\n", num1);
 	printf("So nguyen thu hai la: %d\n", num2);
 	printf("Tong Hai so la: %d\n", sum);
